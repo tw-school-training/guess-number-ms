24eb7bb19bab1888b87b9bb7678c03f0a7e6de2a
zhuming <ming.zhu@thoughtworks.com>
2020-05-13 17:50:40 +0800

    Realize xA0B for Rule of Game

--- src/main/java/com/thoughtworks/guessnumber/Rule.java
+++ src/main/java/com/thoughtworks/guessnumber/Rule.java
@@ -1,7 +1,7 @@
 package com.thoughtworks.guessnumber;
 
 public class Rule {
-    
+
     private String base;
 
     public Rule(String base) {
@@ -9,6 +9,13 @@ public class Rule {
     }
 
     public Result compare(String value) {
-        return Result.builder().build();
+        int numOfA = 0;
+        int numOfB = 0;
+        for (int i = 0; i < value.length(); i++) {
+            if (base.contains(String.valueOf(value.charAt(i)))) {
+                numOfA++;
+            }
+        }
+        return Result.builder().numOfA(numOfA).numOfB(numOfB).build();
     }
 }
--- src/test/java/com/thoughtworks/guessnumber/RuleTest.java
+++ src/test/java/com/thoughtworks/guessnumber/RuleTest.java
@@ -7,10 +7,25 @@ public class RuleTest {
 
     @Test
     public void should_return_0A0B_when_all_numbers_is_wrong() {
+
         String base = "1234";
         Rule rule = new Rule(base);
+
         Result result = rule.compare("5678");
+
         Assert.assertEquals(0, result.getNumOfA());
+        Assert.assertEquals(0, result.getNumOfB());
+    }
+
+    @Test
+    public void should_return_xA0B_when_x_numbers_position_is_right_given_x_numbers_is_right() {
+
+        String base = "1234";
+        Rule rule = new Rule(base);
+
+        Result result = rule.compare("1235");
+
+        Assert.assertEquals(3, result.getNumOfA());
         Assert.assertEquals(0, result.getNumOfB());
     }
 }

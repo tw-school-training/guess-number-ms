b3e6662a16df4105078da5c99e7e265fe57fc46d
zhuming <ming.zhu@thoughtworks.com>
2020-05-14 11:01:58 +0800

    Realize 4 digit for Generator of Game

--- src/main/java/com/thoughtworks/guessnumber/Generator.java
+++ src/main/java/com/thoughtworks/guessnumber/Generator.java
@@ -1,20 +1,17 @@
 package com.thoughtworks.guessnumber;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Generator {
 
-    private List<Integer> baseNumbers = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-
     public String generate() {
-        List<Integer> originalNumbers = new ArrayList<>(baseNumbers);
+        List<Integer> originalNumbers = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
         Random random = new Random();
         StringBuilder sb = new StringBuilder();
         for (int times = 0; times < 4; times++) {
             int index = random.nextInt(originalNumbers.size());
-            sb.append(originalNumbers.remove(index));
+            sb.append(originalNumbers.get(index));
         }
         return sb.toString();
     }

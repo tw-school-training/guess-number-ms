f882dd5e912d07af7000595bc309b466cae85905
zhuming <ming.zhu@thoughtworks.com>
2020-05-14 11:34:06 +0800

    Realize removing repeating numbers

--- src/main/java/com/thoughtworks/guessnumber/Generator.java
+++ src/main/java/com/thoughtworks/guessnumber/Generator.java
@@ -1,17 +1,20 @@
 package com.thoughtworks.guessnumber;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Generator {
 
+    private List<Integer> baseNumbers = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
     public String generate() {
-        List<Integer> originalNumbers = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> originalNumbers = new ArrayList<>(baseNumbers);
         Random random = new Random();
         StringBuilder sb = new StringBuilder();
         for (int times = 0; times < 4; times++) {
             int index = random.nextInt(originalNumbers.size());
-            sb.append(originalNumbers.get(index));
+            sb.append(originalNumbers.remove(index));
         }
         return sb.toString();
     }
--- src/test/java/com/thoughtworks/guessnumber/GeneratorTest.java
+++ src/test/java/com/thoughtworks/guessnumber/GeneratorTest.java
@@ -15,4 +15,22 @@ public class GeneratorTest {
         Assert.assertEquals(4, result.length());
     }
 
+    @Test
+    public void should_not_exist_repeating_numbers_when_generate() {
+        Generator generator = new Generator();
+
+        String result = generator.generate();
+
+        Assert.assertTrue(result, nonRepeatingNumbers(result));
+    }
+
+    private boolean nonRepeatingNumbers(String result) {
+        for (int charIndex = 0; charIndex < result.length(); charIndex++) {
+            char ch = result.charAt(charIndex);
+            if (result.lastIndexOf(ch) != charIndex) {
+                return false;
+            }
+        }
+        return true;
+    }
 }

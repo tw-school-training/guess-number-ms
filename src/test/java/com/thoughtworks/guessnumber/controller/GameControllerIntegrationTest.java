29fc5e88602ed7109035b123a07215b9a0ed40ba
vickerx <vicker23@163.com>
2020-05-26 18:46:52 +0800

    Add Dbunit test

--- src/test/java/com/thoughtworks/guessnumber/controller/GameControllerIntegrationTest.java
+++ src/test/java/com/thoughtworks/guessnumber/controller/GameControllerIntegrationTest.java
@@ -1,12 +1,18 @@
 package com.thoughtworks.guessnumber.controller;
 
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -14,12 +20,17 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        MockitoTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class})
 public class GameControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @DatabaseTearDown(value = "/dbunit/GameControllerIntegrationTest/should_return_game_record_response_when_guess_tear_down.xml")
     public void should_return_game_record_response_when_guess() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/guess").queryParam("user-answer", "1234"))
                 .andExpect(MockMvcResultMatchers.status().isOk())

ac9afb4046719cafd013d8984bd31beebec9b753
vickerx <vicker23@163.com>
2020-05-27 11:39:00 +0800

    Add unit test of findGuessHistory for GameController

--- src/test/java/com/thoughtworks/guessnumber/controller/GameControllerTest.java
+++ src/test/java/com/thoughtworks/guessnumber/controller/GameControllerTest.java
@@ -17,11 +17,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -37,14 +40,7 @@ public class GameControllerTest {
 
     @Test
     public void should_return_game_record_response_when_guess() throws Exception {
-        GameRecord gameRecord = GameRecord.builder()
-                .ticket("ticket")
-                .userGuess("1234")
-                .leftTimes(4)
-                .compareResult("1A2B")
-                .isWinning(false)
-                .id("id")
-                .build();
+        GameRecord gameRecord = buildGameRecord();
         Mockito.when(gameService.guess(Mockito.anyString())).thenReturn(gameRecord);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/guess").queryParam("user-answer", "1234"))
@@ -55,6 +51,31 @@ public class GameControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.isWinning", Matchers.is(gameRecord.isWinning())));
     }
 
+    @Test
+    public void should_return_guess_record_response_when_find_guess_history() throws Exception {
+        List<GameRecord> gameRecords = List.of(
+                buildGameRecord()
+        );
 
+        Mockito.when(gameService.findGameRecords()).thenReturn(gameRecords);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/find-guess-history"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(gameRecords.size())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].userGuess", Matchers.is(gameRecords.get(0).getUserGuess())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].compareResult", Matchers.is(gameRecords.get(0).getCompareResult())));
+    }
+
+    private GameRecord buildGameRecord() {
+        return GameRecord.builder()
+                .ticket("ticket")
+                .userGuess("1234")
+                .leftTimes(4)
+                .compareResult("1A2B")
+                .isWinning(false)
+                .id("id")
+                .build();
+    }
 
 }
